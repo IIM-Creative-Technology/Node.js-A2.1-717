dcf477533f0c53d543882b2f5e6079fee65904b1
jarrel <jarrel.folly-adjon@edu.devinci.fr>
2023-01-26 11:27:12 +0100

    server express and io plus gitignore
    and route template get and post

---
+++ index.js
@@ -0,0 +1,38 @@
+const { Socket } = require("dgram");
+const express = require("express");
+const http = require("http");
+const {Server} = require("socket.io");
+const cors = require("cors");
+
+
+const app  = express()
+
+const port = 3000;
+
+const httpserver = http.createServer(app);
+
+const io = new Server(httpserver, {
+  cors: {
+    origin: "*",
+  }
+});
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({msg: "ok"});
+});
+
+app.post("/", (req, res) => {
+  res.json({msg: "gooooooo"});
+});
+
+
+io.on("connection", (socket) => {
+  console.log(socket);
+})
+
+httpserver.listen(port, () => {
+  console.log(`Server running at http://${port}/`);
+  
+})
